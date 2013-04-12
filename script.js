c0740dff1bb4c25598337f1daafab744c171b6ce
bennylin <bknliem@yahoo.com>
2013-04-12 22:00:22 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+
+// <![CDATA[
+function init_document()
+{
+  var element=document.getElementById("ta");
+	var jv=DoTransliterate(element.value);
+	var pre_view_element=document.getElementById("prev_label");
+	pre_view_element.innerHTML=jv;
+}
+function DoPreview(){
+	var text=$("#inp_txt").val();
+	var trans_text= DoTransliterate(text);
+	$("#prev_label").text(trans_text);
+}
+function DoAppendTransliteration(){
+  var currentText=$("#ta").val();
+
+  var text=$("#inp_txt").val();
+   if(text.toLowerCase()=='clear'){
+   /*typing clear in the inputbox is very tempting.. lets write a hack.*/
+    $("#inp_txt").val('');
+    $("#ta").val('');
+    return;
+  }
+  var trans_text= DoTransliterate(text);
+  var finalText=currentText+' '+trans_text;
+  $("#ta").val(finalText);
+   $("#inp_txt").val('');
+}
+$(document).ready(function () {
+	$('#inp_txt').keydown(function(event) {
+		  if (event.keyCode == '13') {
+		      	DoAppendTransliteration();
+		   }
+	});
+
+	$('#inp_txt').keyup(function() {
+		DoPreview();
+	});
+	DoPreview();
+    });
+// ]]>
